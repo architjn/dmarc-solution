79949ae4d78ea9f8aff28ae287834523e7291108
Archit Jain <archit.jain@kratikal.com>
2019-09-25 22:32:21 +0530

    record fetcher added

---
+++ fetcher.js
@@ -0,0 +1,27 @@
+var dns = require('dns');
+
+var fetch = function (domainName){
+    return new Promise((resolve, reject) => {
+        dns.resolveTxt('_dmarc.' + domainName, (err, records) => {
+            if(err){
+                if(err.message && typeof err.message == 'string' && err.message.startsWith('queryTxt ENOTFOUND'))
+                    return reject('DMARC Record not available');
+                return reject(err.message);
+            } 
+            var record = null;
+            for(var i=0;i<records.length;i++){
+                for(var j=0;j<records[i].length;j++){
+                    if(records[i][j].startsWith('v=DMARC')){
+                        record = records[i][j];
+                        break;
+                    }
+                }
+                if(record != null)
+                    break;
+            }
+            if(record == null) return reject('DMARC Record not available');
+            return resolve(record);
+        });
+    })
+}
+module.exports = fetch;
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 var parser = require('./parser');
+var fetcher = require('./fetcher');
 
-exports.parser = function (dmarcRecord) {
+var recordParser = function (dmarcRecord) {
     return new Promise((resolve, reject) => {
         var result = parser(dmarcRecord);
-        if (result.messages.length) reject(messages);
-        else resolve(result.tags);
+        if (result.messages && result.messages.length) return reject(messages);
+        resolve(result.tags);
     });
 }
+
+var recordFetcher = function (domainName) {
+    return new Promise((resolve, reject) => {
+        return fetcher(domainName)
+            .then(record => {
+                return recordParser(record).then(r => [r, record]);
+            })
+            .then(([data, record]) => {
+                resolve({ record: record, tags: data });
+            })
+            .catch(err => {
+                reject(err);
+            })
+    })
+}
+
+exports.parse = recordParser;
+exports.fetch = recordFetcher;
