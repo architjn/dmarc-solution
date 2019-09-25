f05991c67c180eafe764b0ef9228db0b9230fda3
Archit Jain <archit.jain@kratikal.com>
2019-09-26 00:15:05 +0530

    use tab instead of spaces

--- index.js
+++ index.js
@@ -3,36 +3,36 @@ var fetcher = require('./bin/fetcher');
 var generator = require('./bin/generator');
 
 var recordParser = function (dmarcRecord) {
-    return new Promise((resolve, reject) => {
-        var result = parser(dmarcRecord);
-        if (result.messages && result.messages.length) return reject(result.messages);
-        resolve(result.tags);
-    });
+	return new Promise((resolve, reject) => {
+		var result = parser(dmarcRecord);
+		if (result.messages && result.messages.length) return reject(result.messages);
+		resolve(result.tags);
+	});
 }
 
 var recordFetcher = function (domainName) {
-    return new Promise((resolve, reject) => {
-        return fetcher(domainName)
-            .then(record => {
-                return recordParser(record).then(r => [r, record]);
-            })
-            .then(([data, record]) => {
-                resolve({ record: record, tags: data });
-            })
-            .catch(err => {
-                reject(err);
-            })
-    })
+	return new Promise((resolve, reject) => {
+		return fetcher(domainName)
+			.then(record => {
+				return recordParser(record).then(r => [r, record]);
+			})
+			.then(([data, record]) => {
+				resolve({ record: record, tags: data });
+			})
+			.catch(err => {
+				reject(err);
+			})
+	})
 }
 
 var recordGenerator = function (values) {
-    return new Promise((resolve, reject) => {
-        try {
-            resolve(generator(values))
-        } catch (err) {
-            reject(err.message)
-        }
-    })
+	return new Promise((resolve, reject) => {
+		try {
+			resolve(generator(values))
+		} catch (err) {
+			reject(err.message)
+		}
+	})
 }
 
 exports.parse = recordParser;
